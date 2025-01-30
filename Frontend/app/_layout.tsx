72ed31fc7bd00174e6111d509136568f2584eaab
Michael-Crowther <md.crowther@yahoo.com>
2025-01-30 12:26:48 -0700

    Created Frontend and Backend folders. Got navigation tabs setup. Testing and experimenting with Go on Backend

---
+++ Frontend/app/_layout.tsx
@@ -0,0 +1,96 @@
+import FontAwesome from "@expo/vector-icons/FontAwesome";
+import {
+  DarkTheme,
+  DefaultTheme,
+  NavigationContainer,
+  ThemeProvider,
+} from "@react-navigation/native";
+import { useFonts } from "expo-font";
+import { Stack } from "expo-router";
+import * as SplashScreen from "expo-splash-screen";
+import { useEffect } from "react";
+import "react-native-reanimated";
+import "../global.css";
+
+import { useColorScheme } from "@/Frontend/components/useColorScheme";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import HomeScreen from "./(screens)/HomeScreen";
+import ProfileScreen from "./(screens)/ProfileScreen";
+import SearchScreen from "./(screens)/SearchScreen";
+import AddRecipeScreen from "./(screens)/AddRecipeScreen";
+import { Ionicons } from "@expo/vector-icons";
+
+export {
+  // Catch any errors thrown by the Layout component.
+  ErrorBoundary,
+} from "expo-router";
+
+export const unstable_settings = {
+  // Ensure that reloading on `/modal` keeps a back button present.
+  initialRouteName: "(tabs)",
+};
+
+// Prevent the splash screen from auto-hiding before asset loading is complete.
+SplashScreen.preventAutoHideAsync();
+
+export default function RootLayout() {
+  const [loaded, error] = useFonts({
+    SpaceMono: require("../assets/fonts/SpaceMono-Regular.ttf"),
+    ...FontAwesome.font,
+  });
+
+  // Expo Router uses Error Boundaries to catch errors in the navigation tree.
+  useEffect(() => {
+    if (error) throw error;
+  }, [error]);
+
+  useEffect(() => {
+    if (loaded) {
+      SplashScreen.hideAsync();
+    }
+  }, [loaded]);
+
+  if (!loaded) {
+    return null;
+  }
+
+  return <RootLayoutNav />;
+}
+
+function RootLayoutNav() {
+  const colorScheme = useColorScheme();
+
+  const Tab = createBottomTabNavigator();
+
+  return (
+    <ThemeProvider value={colorScheme === "dark" ? DarkTheme : DefaultTheme}>
+      <Tab.Navigator
+        screenOptions={({ route }) => ({
+          tabBarIcon: ({ color, size }) => {
+            let iconName;
+
+            if (route.name === "Home") {
+              iconName = "home-outline";
+            } else if (route.name === "Search") {
+              iconName = "search-outline";
+            } else if (route.name === "Add Recipe") {
+              iconName = "add-circle-outline";
+            } else if (route.name === "Profile") {
+              iconName = "person-outline";
+            }
+
+            //@ts-ignore
+            return <Ionicons name={iconName} size={size} color={color} />;
+          },
+          tabBarActiveTintColor: "tomato",
+          tabBarInactiveTintColor: "gray",
+        })}
+      >
+        <Tab.Screen name="Home" component={HomeScreen} />
+        <Tab.Screen name="Search" component={SearchScreen} />
+        <Tab.Screen name="Add Recipe" component={AddRecipeScreen} />
+        <Tab.Screen name="Profile" component={ProfileScreen} />
+      </Tab.Navigator>
+    </ThemeProvider>
+  );
+}
